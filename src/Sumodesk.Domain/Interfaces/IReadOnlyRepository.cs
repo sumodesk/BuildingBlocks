6ba3c85024c4cf9bbcb8a4bb34757e7bceb54682
Emilio <emiliogg84@outlook.com>
2024-10-26 12:14:17 -0500

    Add IReadOnlyRepository interface

---
+++ src/Sumodesk.Domain/Interfaces/IReadOnlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sumodesk.Domain.Abstractions;
+
+namespace Sumodesk.Domain.Interfaces;
+
+public interface IReadOnlyRespository<TEntity>
+	where TEntity : class
+{
+	Task<TEntity?> GetOneAsync(QueryFilter<TEntity> filter, CancellationToken cancellationToken = default);
+	Task<IReadOnlyList<TEntity>> GetAllAsync(IncludeProps<TEntity>? includeProps = default, CancellationToken cancellationToken = default);
+	Task<IReadOnlyList<TEntity>> GetFilteredAsync(QueryFilter<TEntity> filter, CancellationToken cancellationToken = default);
+	Task<int> CountAsync(QueryFilter<TEntity>? filter = default, CancellationToken cancellationToken = default);
+	Task<bool> AnyAsync(QueryFilter<TEntity>? filter = default, CancellationToken cancellationToken = default);
+}
