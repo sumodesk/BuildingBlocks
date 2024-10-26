aa38cf88d7f5f6f52ed03411ec495ca44a4fa48e
Emilio <emiliogg84@outlook.com>
2024-10-26 12:05:51 -0500

    Create CommunicationInfoDto.cs

---
+++ src/Sumodesk.DTOs/Shared/CommunicationInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Sumodesk.DTOs.Shared;
+
+public class CommunicationInfoDto
+{
+	public string? FaxNo_ { get; set; }
+	public string? TelExNo_ { get; set; }
+	public string? TelexAnswerBack { get; set; }
+	public string? PhoneNo_1 { get; set; }
+	public string? PhoneNo_2 { get; set; }
+	public string? Email { get; set; }
+}
