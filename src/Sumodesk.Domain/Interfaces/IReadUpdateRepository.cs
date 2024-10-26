7c00105f6c2d188615f62add4c0aa0e9e9f5f0c9
Emilio <emiliogg84@outlook.com>
2024-10-26 12:11:30 -0500

    Add ReadUpdateRepository interface

---
+++ src/Sumodesk.Domain/Interfaces/IReadUpdateRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sumodesk.Domain.Abstractions;
+
+namespace Sumodesk.Domain.Interfaces;
+
+public interface IReadUpdateRepository<TEntity>
+	where TEntity : class, IEntityRoot
+{
+	bool EnableTracking { get; set; }
+	Task<TEntity?> GetByIdAsync(int id, IncludeProps<TEntity>? includeProps = default, CancellationToken cancellationToken = default);
+	Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+}
